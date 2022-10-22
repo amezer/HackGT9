8de90f53816dde2be3dd6f9a01b34d0ecbd9042e
amezer <jocelyn.her.ho@gmail.com>
2022-10-22 07:09:16 -0400

    added function to popup.html btns

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
-chrome.storage.sync.get('positionLeft', function(result) {
-    console.log(result)
-    if (result.positionLeft == undefined) {
-        posLeft = '100px'
-        console.log('null, but set to 100, 400' + posLeft)
-    } else {
-        posLeft = result.positionLeft;
-        console.log('LeftValue currently is ' + result.positionLeft);
-    }
-    
-});
-
-
 let mark = document.getElementById('questionMark');
 let block = document.getElementById('questionHover');
 mark.style.cursor = "pointer"
 mark.onmouseenter = function(){block.style.opacity = 1;};
 mark.onmouseleave = function(){block.style.opacity = 0;};
+
+var dropBtn = document.getElementById('drop');
+dropBtn.onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log("sending drop to content...");
+        chrome.tabs.sendMessage(tabs[0].id, {message: "drop"});
+    });
+}
+
+var feedBtn = document.getElementById('feed');
+feedBtn.onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log("sending carrot to content...");
+        chrome.tabs.sendMessage(tabs[0].id, {message: "carrot"});
+    });
+}
